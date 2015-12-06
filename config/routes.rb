134bf276383af052cf0758993e91d6172ba4ca10
kenta <kenta@docomo.ne.jp>
2015-12-06 12:59:28 +0000

    merge stash

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,10 @@ Rails.application.routes.draw do
   resources :profiles
   devise_for :users, path_names: { sign_in: "login", sign_out: "logout"},
     controllers: { omniauth_callbacks: "omniauth_callbacks" }
-  
-
   get 'home/top'
-
   resources :izakaya_profiles
 
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
